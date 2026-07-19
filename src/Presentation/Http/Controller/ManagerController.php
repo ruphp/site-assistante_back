61b945f6d6fc4da01c26f1885ac412b510bfdddf
ruphp <ruphp@users.noreply.github.com>
2026-07-19 17:45:13 +0500

    Fix manager menu access guard

--- src/Presentation/Http/Controller/ManagerController.php
+++ src/Presentation/Http/Controller/ManagerController.php
@@ -2,21 +2,11 @@
 
 namespace app\Presentation\Http\Controller;
 
-use app\Application\Panel\ClientPanelMenuService;
-use app\Presentation\Yii\Widget\LeftMenu;
 use Yii;
 use yii\filters\AccessControl;
 
 class ManagerController extends SmartiusController
 {
-    protected function leftMenu(array $lists): string
-    {
-        return LeftMenu::widget([
-            'list' => $this->clientPanelMenu()->baseMenu(),
-            'lists' => $lists,
-        ]);
-    }
-
     public function behaviors(): array
     {
         Yii::$app->cache->flush();
@@ -27,22 +17,10 @@ class ManagerController extends SmartiusController
                     [
                         'allow'         => true,
                         'roles'         => ['manager'],
-                        'matchCallback' => function () {
-                            $publicKey = Yii::$app->user->identity->getPublicKey();
-                            $this->leftMenu($this->clientPanelMenu()->moduleMenusForClient($publicKey));
-
-                            return true;
-                        },
                     ],
                 ],
             ]
         ];
 
     }
-
-    private function clientPanelMenu(): ClientPanelMenuService
-    {
-        return Yii::$container->get(ClientPanelMenuService::class);
-    }
-
 }
